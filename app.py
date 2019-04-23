436ee2e2308075ec8b0ffd8c6e541382e178625b
francis <fc373745@gmail.com>
2019-04-23 13:52:48 -0700

    added configuration

--- app.py
+++ app.py
@@ -2,9 +2,17 @@ import os
 
 from flask import Flask, jsonify
 from flask_restful import Api
+from dotenv import load_dotenv
 from db import db
 
 app = Flask(__name__)
+
+load_dotenv(".env", verbose=True)
+app.config.from_object("default_config")  # load default configs from default_config.py
+app.config.from_envvar(
+    "APPLICATION_SETTINGS"
+)  # override with config.py (APPLICATION_SETTINGS points to config.py)
+
 api = Api(app)
 
 if __name__ == "__main__":
