bf72db953ef8ce46c26d4e949ef85791d1ee2242
francis <fc373745@gmail.com>
2019-04-23 16:16:08 -0700

    added secret key and got basic functionality with authentication to work with argon2 hashing

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 from db import db
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
+from resources.user import UserRegister, UserLogin
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 
@@ -14,6 +16,8 @@ app.config.from_object("default_config")  # load default configs from default_co
 app.config.from_envvar(
     "APPLICATION_SETTINGS"
 )  # override with config.py (APPLICATION_SETTINGS points to config.py)
+app.secret_key = os.environ.get("SECRET_KEY")
+jwt = JWTManager(app)
 
 api = Api(app)
 
@@ -21,6 +25,10 @@ api = Api(app)
 @app.before_first_request
 def create_tables():
     db.create_all()
+
+
+api.add_resource(UserRegister, "/register")
+api.add_resource(UserLogin, "/login")
 
 
 if __name__ == "__main__":
--- resources/user.py
+++ resources/user.py
@@ -1,9 +1,41 @@
-from ma import ma
+from flask_restful import Resource
+from flask import request
+from flask_jwt_extended import create_access_token, create_refresh_token
 from models.user import UserModel
+from schemas.user import UserSchema
+from argon2 import PasswordHasher, exceptions
+
+user_schema = UserSchema()
+ph = PasswordHasher()
+
+class UserRegister(Resource):
+    @classmethod
+    def post(cls):
+        user_json = request.get_json()
+        password = ph.hash(user_json["password"])
+        user_json["password"] = password
+        user = user_schema.load(user_json)
+
+        if UserModel.find_by_username(user.username):
+            return {"message": "username exists"}, 400
+
+        user.save_to_db()
+
+        return {"message": "you are now registered"}, 201
 
 
-class UserSchema(ma.ModelSchema):
-    class Meta:
-        model = UserModel
-        load_only = ("password",)
-        dump_only = ("id",)
+class UserLogin(Resource):
+    @classmethod
+    def post(cls):
+        user_json = request.get_json()
+        user_data = user_schema.load(user_json)
+
+        user = UserModel.find_by_username(user_data.username)
+
+        try:
+            if user and ph.verify(user.password, user_data.password):
+                access_token = create_access_token(identity=user.id, fresh=True)
+                refresh_token = create_refresh_token(user.id)
+                return {"access_token": access_token, "refresh_token": refresh_token}, 200
+        except exceptions.VerifyMismatchError:
+            return {"message": "invalid credentials"}, 401
