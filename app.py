a1ef750d6208424a0f78b16c82c52002250a6734
francis <fc373745@gmail.com>
2019-04-23 13:54:55 -0700

    added env

--- app.py
+++ app.py
@@ -15,6 +15,12 @@ app.config.from_envvar(
 
 api = Api(app)
 
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
 if __name__ == "__main__":
     db.init_app(app)
     app.run(port=5000, debug=True)
