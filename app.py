63d7a9f6e5e37f7ebf465be1b083d2bae2dee64d
francis <fc373745@gmail.com>
2019-04-23 13:40:09 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import os
+
+from flask import Flask, jsonify
+from flask_restful import Api
+
+
+app = Flask(__name__)
+api = Api(app)
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
