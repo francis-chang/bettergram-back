59c8cc523e4147e8bac424c845c5160ebeb76436
francis <fc373745@gmail.com>
2019-04-23 13:46:23 -0700

    added sqlalch

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ import os
 
 from flask import Flask, jsonify
 from flask_restful import Api
-
+from db import db
 
 app = Flask(__name__)
 api = Api(app)
 
 if __name__ == "__main__":
+    db.init_app(app)
     app.run(port=5000, debug=True)
