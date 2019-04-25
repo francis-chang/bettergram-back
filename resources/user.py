2d8117dec79407684e58f409f3138def07aeb508
francis <fc373745@gmail.com>
2019-04-25 00:51:03 -0700

    added Token refreshing endpoint

--- resources/user.py
+++ resources/user.py
@@ -1,6 +1,6 @@
 from flask_restful import Resource
 from flask import request, make_response, render_template
-from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_refresh_token_required, get_jwt_identity
 from models.user import UserModel
 from schemas.user import UserSchema
 from argon2 import PasswordHasher, exceptions
@@ -76,3 +76,11 @@ class UserVerify(Resource):
 
         except:
             return {"message": "confirmation resend fail"}, 500
+
+class TokenRefresh(Resource):
+    @classmethod
+    @jwt_refresh_token_required
+    def post(cls):
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user, fresh=False)
+        return {"access_token": new_token}, 200
