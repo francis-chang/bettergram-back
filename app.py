96ff48eb3c86b2d453160d7412609da3d587ebda
francis <fc373745@gmail.com>
2019-04-25 06:54:13 -0700

    oauth finished

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 import os
-
 from flask import Flask, jsonify
 from flask_restful import Api
-
 from dotenv import load_dotenv
+
+load_dotenv(".env", verbose=True)
+
 from db import db
 from oauth import oauth
 from ma import ma
-
 from resources.user import UserRegister, UserLogin, UserVerify, TokenRefresh
 from flask_jwt_extended import JWTManager
+from resources.github_login import GithubLogin, GithubAuthorize
 
 app = Flask(__name__)
 
 
-load_dotenv(".env", verbose=True)
 app.config.from_object("default_config")  # load default configs from default_config.py
 app.config.from_envvar(
     "APPLICATION_SETTINGS"
@@ -34,6 +34,8 @@ api.add_resource(UserRegister, "/register")
 api.add_resource(UserLogin, "/login")
 api.add_resource(UserVerify, "/confirmation/<int:user_id>")
 api.add_resource(TokenRefresh, "/refresh")
+api.add_resource(GithubLogin, "/login/github")
+api.add_resource(GithubAuthorize, "/login/github/authorized")
 
 if __name__ == "__main__":
     db.init_app(app)
--- models/user.py
+++ models/user.py
@@ -11,7 +11,7 @@ class UserModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
-    password = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(80))
     email = db.Column(db.String(80), nullable=False, unique=True)
     activated = db.Column(db.Boolean, nullable=False, default=False)
 
